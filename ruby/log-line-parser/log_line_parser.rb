5d6a71535d7b0c905e8bfd84bb18af6d3dda7ad7
amirulmenjeni <amirulmenjeni@gmail.com>
2022-10-07 22:37:42 +0800

    Solve ruby/log-line-parser

---
+++ ruby/log-line-parser/log_line_parser.rb
@@ -0,0 +1,17 @@
+class LogLineParser
+  def initialize(line)
+    @line = line
+  end
+
+  def message
+    @line.split(":")[1].strip
+  end
+
+  def log_level
+    @line.split(":")[0].strip.downcase[1..-2]
+  end
+
+  def reformat
+    "#{message} (#{log_level})"
+  end
+end
